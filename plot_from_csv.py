2632ed438ab0cea6e6af9bab07fe7d65c51499cc
Karl <ksyoungnm@gmail.com>
2019-07-31 15:24:15 -0700

    Adding old sim files

---
+++ plot_from_csv.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def init():
+    ax.set_ylim(-100, 60)
+    ax.set_xlim(0, 300)
+    del t[:]
+    del v[:]
+    line.set_data(t, v)
+    return line,
+
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=2)
+ax.grid()
+t, v = [], []
+
+
+def animate(i):
+    # update the data
+    data = pd.read_csv('data.csv')
+    t = data['time']
+    v = data['voltage']
+
+
+    xmin, xmax = ax.get_xlim()
+    l_edge = xmax-50
+    recent_t = t[len(t)-1] 
+
+    if recent_t >= l_edge:
+        ax.set_xlim(xmin+1, xmax+1)
+        ax.figure.canvas.draw()
+    line.set_data(t, v)
+
+    return line,
+
+# ani = animation.FuncAnimation(fig, animate, interval=1, init_func=init)
+data = pd.read_csv('data.csv')
+t=data['time']
+v=data['voltage']
+plt.plot(t,v)
+plt.show()
+
